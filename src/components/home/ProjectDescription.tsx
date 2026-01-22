d8038ed855ba927d0b1344d9a11f26d611aa2ae1
MUHAMMED SHAREEF M <shareefmottath@gmail.com>
2026-01-22 20:25:45 +0530

    code updation

--- src/components/home/ProjectDescription.tsx
+++ src/components/home/ProjectDescription.tsx
@@ -3,14 +3,14 @@ import signature from '../../assets/signature.png';
 
 export const ProjectDescription = () => {
     return (
-        <section className="py-24 px-6 bg-background">
-            <div className="max-w-4xl mx-auto text-center">
-                
+        <section className="bg-background">
+            <div className="mx-auto text-center">
 
-                <div className="mt-12 flex justify-center">
-                    <img 
-                        src={signature} 
-                        alt="Signature" 
+
+                <div className="flex justify-center">
+                    <img
+                        src={signature}
+                        alt="Signature"
                         className="h-24 opacity-80"
                     />
                 </div>
